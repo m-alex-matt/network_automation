7a0e23d00e31e3c37f2064b101fb90045a3fbd57
m-alex-matt <m.alex.matt@gmail.com>
2019-02-24 20:44:36 -0500

    adding bkup.py file

---
+++ netmiko-py/netmiko_bkup.py
@@ -0,0 +1,38 @@
+# Learning Python 2018 / Week 6, Netmiko Intro and Basics
+from netmiko import Netmiko
+import datetime
+
+CUR_TIME = str(datetime.datetime.today().isoformat()[:-7])
+CUR_DIR = '/home/michael/py-scripts/network-py/network_automation/netmiko-py'
+
+def backup(net_conn, hostn):
+    net_conn.enable()
+    command = "show running-config"
+    output = net_conn.send_command(command)
+    try:
+        with open(f'{CUR_DIR}/dev_conf/{hostn}_conf/{hostn}.config', mode='x') as f:
+            f.write(output)
+    except FileExistsError:
+        with open(f'{CUR_DIR}/dev_conf/{hostn}_conf/{hostn}.config', mode='r') as f:
+            with open(f'{CUR_DIR}/dev_conf/{hostn}_conf/{hostn}.config_{CUR_TIME}', mode='w') as f_old:
+                f_old.write(f.read())
+        with open(f'{CUR_DIR}/dev_conf/{hostn}_conf/{hostn}.config', mode='w') as f:
+            f.write(output)
+
+def main():
+    my_device = {
+        'host': '',
+        'username': 'user',
+        'password': 'password',
+        'secret': 'password',
+        'device_type': 'cisco_ios'
+    }   
+
+    my_device['host'] = input("What is the IP address of your device? ")
+    host_name = input("What is the hostname of your device? ")
+    net_conn = Netmiko(**my_device)
+
+    backup(net_conn, host_name)
+
+if __name__ == '__main__':
+    main()
